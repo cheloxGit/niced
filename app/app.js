263c36f1d76f24f6aa55b3d55441f23752fdb8e3
Marcelo <chelazo@gmail.com>
2017-06-01 22:27:02 -0400

    Nice List Full Name

--- app/app.js
+++ app/app.js
@@ -1,16 +1,41 @@
-(function() {
+(function(angular) {
 'use strict';
 
-var angular_app = angular.module('niceDApp', ['smart-table']);
+var angular_app = angular.module('niceDApp', ['smart-table','ngRoute'])
 
-    angular_app.controller('MainController', function ($scope, $http) {
+// configure our routes
+angular_app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+      // route for the home page
+      .when('/', {
+          templateUrl : '/components/home/listUsers.html',
+          controller  : 'CtrlMain'
+      })
+
+      // route for the about page
+      .when('/listRepos/:login_n', {
+          templateUrl : '/components/home/listRepos.html',
+          controller  : 'CtrlUsers'
+      })
+
+      .otherwise({
+        redirectTo: '/'
+      });
+      // Add HTML5 History API support
+      $locationProvider.html5Mode(true);
+});
+
+    angular_app.controller('CtrlMain', function ($scope, $http) {
       $scope.formData = {};
+
+
 
       $http.get('/api/getUser')
         .then(function (response){
           $scope.data_user = angular.fromJson(response['data']);
         });
-      });
+    })
+
 
     angular_app.factory('DataService', function($http, $q){
       function DataService(){
@@ -23,11 +48,32 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
 
           $http.get('/api/getUser/')
             .then (function (response){
-              self.user = angular.fromJson(response['data']);
+              self.user = response['data'];
+              deferred.resolve(angular.fromJson(response['data']));
+            });
+          return deferred.promise;
+        }
+
+        self.getDetailUser = function (resp){
+          var deferred = $q.defer();
+          // .then (function (response){
+          //   $http.get('/api/getDetailUser/'+encodeURIComponent(self.user))
+          //     .then (function (resp){
+          //       self.detail_user = angular.fromJson(resp['data']);
+          //       deferred.resolve(angular.fromJson(resp['data']));
+          //     })
+          // });
+          // console.log('resp: ');
+          // console.log(resp);
+          resp = JSON.stringify(resp);
+
+          $http.get('/api/getDetailUser/'+encodeURIComponent(resp))
+            .then(function (response){
+              self.repo = angular.fromJson(response['data']);
               deferred.resolve(angular.fromJson(response['data']));
             });
 
-          return deferred.promise;
+            return deferred.promise;
         }
 
         self.getRepo = function (login_name){
@@ -42,25 +88,21 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
             return deferred.promise;
         }
 
-        self.getDetailUser = function (login_name){
-          var deferred = $q.defer();
-          console.log('login_name app.js: ');
-          console.log(login_name);
-          $http.get('/api/getDetailUser/'+login_name)
-            .then(function (response){
-              self.repo = angular.fromJson(response['data']);
-              deferred.resolve(angular.fromJson(response['data']));
-            });
 
-            return deferred.promise;
-        }
       }
 
       return new DataService();
     });
 
-    angular_app.controller('CtrlUsers',['$scope','$http','DataService', function($scope, $http, DataService){
+    angular_app.controller('CtrlUsers',['$scope','$http','$routeParams', 'DataService', function($scope, $http, $routeParams, DataService){
       $scope.formData = {};
+
+      // $scope.login_n = $routeParams.login_n;
+      // $scope.getDetailUser = function (login_name){
+      //   console.log('login name getDetailUser: ');
+      //   console.log(login_name);
+      //
+      // }
 
       this.getUsers = function(){
         // this.tab = 2;
@@ -69,7 +111,30 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
         DataService.getUser()
           .then(
             function (data){
-              $scope.rowCollection = angular.fromJson(data);
+              // var data_det = angular.fromJson(data);
+              // var full_name = this.getDetailUser(data_det['']);
+              // resp = data;
+              // $scope.rowCollection = angular.fromJson(data);
+
+              DataService.getDetailUser(data)
+                .then(
+                  function (resp){
+                    // $scope.rowCollectionRepos = angular.fromJson(data);
+                    // console.log(angular.fromJson(data));
+                    // var data_det_usr = angular.fromJson(data);
+                    $scope.showDetailUser = true;
+                    console.log('angular.fromJson(resp)************: ');
+                    console.log(angular.fromJson(resp));
+                    $scope.rowCollection = angular.fromJson(resp);
+                    // return $scope.name_retrieve = data_det_usr['name'];
+                    // return angular.fromJson(data['name']);
+                    // $scope.showListUsers = false;
+                    // $scope.showListRepos = true;
+                  },
+                  function (result){
+                    console.log('Failes, result is:'+ result);
+                  }
+              );
               $scope.showListUsers = true;
             },
             function (result){
@@ -77,11 +142,26 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
             }
           );
       }
+      this.getUsers();
+      // this.getDetailUser();
+    }])
+    .directive('myDirective', function(){
+        return {
+            restrict: 'E',
+            template: "<div><button ng-click='updateparent({person: usrPerson})'>click</button></div>",
+            replace: true,
+            scope: { usrPerson: '=', updateparent: '&' }
+        }
+    });
 
 
-      this.showTable = function (login_name){
+    angular_app.controller('CtrlRepos',['$scope','$http','$routeParams','DataService', function($scope, $http, $routeParams, DataService){
+      $scope.formData = {};
+      // $scope.login_n = $routeParams.login_n;
 
-        DataService.getRepo(login_name)
+      this.showTable = function (){
+
+        DataService.getRepo($routeParams.login_n)
           .then(
             function (data){
               $scope.rowCollectionRepos = angular.fromJson(data);
@@ -95,6 +175,14 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
             }
         );
       }
+
+      this.showTable();
+      // this.getDetailUser();
+    }]);
+
+    angular_app.controller('CtrlDetailUser',['$scope','$http','DataService', function($scope, $http, $routeParams, DataService){
+      $scope.formData = {};
+      // $scope.login_n = $routeParams.login_n;
 
       this.getDetailUser = function (login_name){
         console.log('login name getDetailUser: ');
@@ -117,8 +205,9 @@ var angular_app = angular.module('niceDApp', ['smart-table']);
         );
       }
 
-      this.getUsers();
+      this.getDetailUser();
       // this.getDetailUser();
-
     }]);
-})();
+
+
+})(window.angular);
